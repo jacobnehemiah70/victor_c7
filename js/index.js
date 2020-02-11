07e09730b5d1243ce64c2de186586917e160d923
victor chizoba <victorchizoba30@gmail.com>
2020-02-10 23:09:33 -0800

    my commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// var a = 10;
+// var b = 20;
+// var c = a+b;
+// console.log(c);
+
+// var name = 'Luka';
+// var age = '10';
+// var location = 'Bokkos';
+
+// console.log('My Name '+ name +' and I am '+ age +' years old and I stay in '+ location +'.');
+
+// console.log(`my name is ${name} and i am ${age} old my location is ${location}`);
+
+// var a = 2;
+// var b = 12;
+// var c = 6;
+// // var d = (b+c);
+// var x = a*(b + c);
+// var y = x*.16
+// // var y = x ^ 0.5;
+// // console.log(x);
+// console.log(x);
+// console.log(y);
+
+var a = 1;
+var b = -5;
+var c = 6;
+var p = a*2;
+var q = -(b);
+var x = Math.sqrt((b*b) - 4*a*c);
+var y = (q + x)/p;
+var z = (q - x)/p;
+console.log(x);
+console.log(p);
+console.log(q);
+console.log(y);
+console.log(z);
